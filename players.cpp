8f06c8a659c0e60a716e8f57c82ba528324e7fc9
Samuel <45127751+SamuelKun@users.noreply.github.com>
2019-10-16 22:10:18 +0100

    Delete players.cpp

--- players.cpp
+++
@@ -1,9 +0,0 @@
-/*
-#include <string>
-#include "player.h"
-
-using namespace std;
-
-Player::Player(string name, int bday, int bmonth, int byear, enumPosition pos, string club) : Person(name,bday,bmonth,byear), pos(pos), club(club) {}
-
-*/
