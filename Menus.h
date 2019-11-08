492e23e64d35c46d9da6569a0eb7187a6b61acb0
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2019-11-08 15:19:20 +0000

    Adding documentation

--- Menus.h
+++ Menus.h
@@ -2,11 +2,43 @@
 
 using namespace std;
 
-int menu_principal();
-int menu_jogadores();
-int menu_equipa_tecnica();
+/**
+ * This is the main menu which allows you to choose what you want to with this application
+ * @return
+ */
+int mainMenu();
+/**
+ *
+ * @return
+ */
 
-int menu_staff();
-int menu_players();
+
+
+/**
+ * This menu shows the app creators
+ * @return goes back to the main menu
+ */
 int info_app();
+
+int test();
+/**
+ * This menu allows you to view and change staff info
+ * @return creates or views staff members, or returns to the main menu
+ */
+int menu_staff();
+/**
+ * This menu allows you to view and change player info
+ * @return creates or views players, or returns to the main menu
+ */
+int menu_players();
+/**
+ * This menu refers to all tournaments
+ * It allows you to view tournaments or create new ones
+ * @return creates or views tournaments, or returns to the main menu
+ */
+int menu_tournaments();
+/**
+ * This menu allows you to move between menus in order to use the app properly
+ * @return chooses between different menus or exiting the app
+ */
 int mainMenu();
--- Person.h
+++ Person.h
@@ -5,14 +5,30 @@
 
 using namespace std;
 
+//!
+/*! Class for a person
+ *
+ */
 class Person {
 private:
-	string name;
-	Date birth;
+	string name; /*!< Person's name */
+	Date birth;  /*!< Person's date of birth */
 public:
+    //! A constructor
+    //! \param name creates a person with this name
+    //! \param birth that person's birthday
 	Person(string name, Date birth);
+    //! Get Method
+    //! \return person's name
 	string getName() const;
+	//! Get Method
+	//! \return person's birthday
 	Date getBirthday() const;
+    //! A virtual member
+    /*! Shows person's info:
+     *  Shows name
+     *  Shows birthday
+     */
 	virtual void info() const;
 	//virtual FUNÇÃO DE CADA UM IMPLEMENTAR DEPOIS
 };
