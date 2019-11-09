f97362efa471af01b9f7077c84cd375bac79afce
Samuel <diogosamuelfernandes@outlook.com>
2019-11-09 19:40:42 +0000

    utils

--- Competion.h
+++ Competion.h
@@ -17,6 +17,7 @@ public:
     Competion(string name, vector<Player *> called, vector<Game *> team_games, Date start, Date end);
     string getCompetionName() const;
     void payPlayers();
+    bool getPaid() const;
     vector<Player *> getCalled() const;
     vector<Game *> getGames() const;
     friend ostream & operator<<(ostream &out, const Competion &comp);
