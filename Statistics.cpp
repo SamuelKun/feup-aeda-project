55c4bfa4cfaaf31dfa26b39c00f46bb552d7f3b4
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2019-11-10 14:43:31 +0000

    Statistics done

--- Statistics.cpp
+++ Statistics.cpp
@@ -1,131 +1,81 @@
 #include "Statistics.h"
 
 
-Statistics::Statistics(int goalsScored, int goalAttempts, int shotsOnGoal, int shotsOffGoal, float kmTravelled,int yellowCards, int redCards, int injured, int freeKicks, int cornerKicks):
-        goals_scored(goalsScored),goal_attempts(goalAttempts),shots_on_goal(shotsOnGoal),
-        shots_off_goal(shotsOffGoal),km_travelled(kmTravelled),yellow_cards(yellowCards),
-        red_cards(redCards),injured(injured),free_kicks(freeKicks),
-        corner_kicks(cornerKicks){}
-    
+Statistics::Statistics(int goalsScored, int shots, int ballPossession, float kmTravelled, int yellowCards, int redCards,int injured, int freeKicks, int cornerKicks) :
+    goals_scored(goalsScored), shots(shots),
+    ball_possession(ballPossession), km_travelled(kmTravelled),
+    yellow_cards(yellowCards), red_cards(redCards),
+    injured(injured), free_kicks(freeKicks),
+    corner_kicks(cornerKicks) {}
+
 int Statistics::getGoalsScored() const {
     return goals_scored;
 }
 
-
-int Statistics::getGoalAttempts() const {
-    return goal_attempts;
+void Statistics::setGoalsScored(int goalsScored) {
+    goals_scored = goalsScored;
 }
 
-void Statistics::addGoalAttempts(int goalAttempts) {
-    goal_attempts += goalAttempts;
+int Statistics::getShots() const {
+    return shots;
 }
 
-int Statistics::getShotsOnGoal() const {
-    return shots_on_goal;
+void Statistics::setShots(int shots) {
+    Statistics::shots = shots;
 }
 
-void Statistics::addShotsOnGoal(int shotsOnGoal) {
-    shots_on_goal += shotsOnGoal;
+int Statistics::getBallPossession() const {
+    return ball_possession;
 }
 
-int Statistics::getShotsOffGoal() const {
-    return shots_off_goal;
+void Statistics::setBallPossession(int ballPossession) {
+    ball_possession = ballPossession;
 }
-
-void Statistics::addShotsOffGoal(int shotsOffGoal) {
-    shots_off_goal += shotsOffGoal;
-}
-
 
 float Statistics::getKmTravelled() const {
     return km_travelled;
 }
 
-void Statistics::addKmTravelled(float kmTravelled) {
-    km_travelled += kmTravelled;
+void Statistics::setKmTravelled(float kmTravelled) {
+    km_travelled = kmTravelled;
 }
 
 int Statistics::getYellowCards() const {
     return yellow_cards;
 }
 
-void Statistics::addYellowCards(int yellowCards) {
-    yellow_cards += yellowCards;
+void Statistics::setYellowCards(int yellowCards) {
+    yellow_cards = yellowCards;
 }
 
 int Statistics::getRedCards() const {
     return red_cards;
 }
 
-void Statistics::addRedCards(int redCards) {
-    red_cards += redCards;
+void Statistics::setRedCards(int redCards) {
+    red_cards = redCards;
 }
 
 int Statistics::getInjured() const {
     return injured;
 }
 
-void Statistics::addInjured(int injured) {
-    Statistics::injured += injured;
+void Statistics::setInjured(int injured) {
+    Statistics::injured = injured;
 }
 
 int Statistics::getFreeKicks() const {
     return free_kicks;
 }
 
-void Statistics::addFreeKicks(int freeKicks) {
-    free_kicks += freeKicks;
+void Statistics::setFreeKicks(int freeKicks) {
+    free_kicks = freeKicks;
 }
 
-
-Colective_Statistics::Colective_Statistics(int ballPossession, int goalsScored, int goalAttempts, int shotsOnGoal, int shotsOffGoal,float kmTravelled, int yellowCards, int redCards, int injured, int freeKicks, int cornerKicks):
-    Statistics(goalsScored,goalAttempts,shotsOnGoal,shotsOffGoal,kmTravelled,yellowCards,redCards,injured,freeKicks,cornerKicks),
-    ball_possession(ballPossession) {}
-
-int Colective_Statistics::getBallPossession() const {
-    return ball_possession;
+int Statistics::getCornerKicks() const {
+    return corner_kicks;
 }
 
-void Colective_Statistics::addBallPossession(int ballPossession) {
-    ball_possession += ballPossession;
-}
-
-void Colective_Statistics::info() const {
-    cout << " goals_scored: " << goals_scored << endl;
-    cout << " goal_attempts: " << goal_attempts << endl;
-    cout << " shots_on_goal: " << shots_on_goal << endl;
-    cout << " shots_off_goal: " << shots_off_goal << endl;
-    cout << " km_travelled: " << km_travelled << endl;
-    cout << " yellow_cards: " << yellow_cards << endl;
-    cout << " red_cards: " << red_cards << endl;
-    cout << " injured: " << injured << endl;
-    cout << " free_kicks: " << free_kicks << endl;
-    cout << " corner_kicks: " << corner_kicks << endl;
-    cout << " ball_possession: " << ball_possession << endl;
-}
-
-Individual_Statistics::Individual_Statistics(int minutesPlayed, int goalsScored, int goalAttempts, int shotsOnGoal,int shotsOffGoal, float kmTravelled, int yellowCards, int redCards, int injured, int freeKicks, int cornerKicks):
-    Statistics(goalsScored,goalAttempts,shotsOnGoal,shotsOffGoal,kmTravelled,yellowCards,redCards,injured,freeKicks,cornerKicks),
-    minutes_played(minutesPlayed){}
-
-int Individual_Statistics::getMinutesPlayed() const {
-    return minutes_played;
-}
-
-void Individual_Statistics::addMinutesPlayed(int minutesPlayed) {
-    minutes_played += minutesPlayed;
-}
-
-void Individual_Statistics::info() const {
-    cout << " goals_scored: " << goals_scored << endl;
-    cout << " goal_attempts: " << goal_attempts << endl;
-    cout << " shots_on_goal: " << shots_on_goal << endl;
-    cout << " shots_off_goal: " << shots_off_goal << endl;
-    cout << " km_travelled: " << km_travelled << endl;
-    cout << " yellow_cards: " << yellow_cards << endl;
-    cout << " red_cards: " << red_cards << endl;
-    cout << " injured: " << injured << endl;
-    cout << " free_kicks: " << free_kicks << endl;
-    cout << " corner_kicks: " << corner_kicks << endl;
-    cout << " minutes_played: " << minutes_played << endl;
+void Statistics::setCornerKicks(int cornerKicks) {
+    corner_kicks = cornerKicks;
 }
--- Statistics.h
+++ Statistics.h
@@ -5,32 +5,41 @@ using namespace std;
 
 /// \brief This class refers to the individual statistics of a player during a match
 class Statistics {
-protected:
-    int goals_scored;  /// \brief Number of goals scored
-    int shots;         /// \brief Number of shots on goal
-    int ball_possession;/// \brief Ball possession as percentage
-    int yellow_cards;  /// \brief Number of yellow cards received
-    int red_cards;     /// \brief Number of red cards received
-    int injured;       /// \brief 1 if injured, 0 otherwise
-    int free_kicks;    /// \brief Number of free kicks
-    int corner_kicks;  /// \brief Number of corners
 public:
-    Statistics( );
-    int getGoalsScored() const;
-    int getShots() const;
-    float getKmTravelled() const;
-    int getYellowCards() const;
-    int getRedCards() const;
-    int getInjured() const;
-    int getFreeKicks() const;
+    Statistics(int goalsScored = 0, int shots = 0, int ballPossession = 0, float kmTravelled = 0, int yellowCards = 0, int redCards = 0,
+               int injured = 0, int freeKicks = 0, int cornerKicks = 0);
 
+    int getGoalsScored() const;
     void setGoalsScored(int goalsScored);
-    void setShots(int goalAttempts);
+    int getShots() const;
+    void setShots(int shots);
+    int getBallPossession() const;
+    void setBallPossession(int ballPossession);
+    float getKmTravelled() const;
     void setKmTravelled(float kmTravelled);
+    int getYellowCards() const;
     void setYellowCards(int yellowCards);
+    int getRedCards() const;
     void setRedCards(int redCards);
+    int getInjured() const;
     void setInjured(int injured);
+    int getFreeKicks() const;
     void setFreeKicks(int freeKicks);
+    int getCornerKicks() const;
+    void setCornerKicks(int cornerKicks);
+
+protected:
+    int goals_scored;   /// \brief Number of goals scored
+    int shots;          /// \brief Number of shots on goal
+    int ball_possession;/// \brief Ball possession as percentage
+    float km_travelled; /// \brief Number of kilometres travelled
+    int yellow_cards;   /// \brief Number of yellow cards received
+    int red_cards;      /// \brief Number of red cards received
+    int injured;        /// \brief 1 if injured, 0 otherwise
+    int free_kicks;     /// \brief Number of free kicks
+    int corner_kicks;   /// \brief Number of corners
+public:
+
 };
 
 #endif
