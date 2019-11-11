6e93d7e413d096392031b68a0b6618178beabe40
Samuel <diogosamuelfernandes@outlook.com>
2019-11-11 17:48:50 +0000

    Input utils

--- input_utils.h
+++ input_utils.h
@@ -2,12 +2,13 @@
 #define AEDA_INPUT_UTILS_H
 
 #include <iostream>
-
-
+#include <string>
 
 template <class form>
-void failInput(form input) {
-    while (cin.fail()) {
+void failInput(form input)
+{
+    while (cin.fail())
+    {
         cin.clear();
         cin.ignore();
         cin.ignore(1000, '\n');
@@ -16,15 +17,15 @@ void failInput(form input) {
     }
 }
 
-void waitInput() {
+void waitInput(){
     string waiting;
     cout << "Press any key to continue: " << endl;
     getline(cin,waiting);
 }
 
-void checkPosition(string &position) {
+void checkPosition(string &position){
     while(position != "Goalkeeper" && position != "Defender" &&
-          position != "Midfielder" && position != "Forward") {
+          position != "Midfielder" && position != "Forward"){
         cin.clear();
         cin.ignore(1000, '\n');
         cout << "Not a valid position. Please reenter: " << endl;
@@ -32,5 +33,6 @@ void checkPosition(string &position) {
         cin >> position;
     }
 }
+
 
 #endif //FEUP_AEDA_PROJECT_INPUT_UTILS_H
