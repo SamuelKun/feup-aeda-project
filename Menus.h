a309101cea6e8571e462dc7555c3bfb9003d0f10
Samuel <diogosamuelfernandes@outlook.com>
2019-11-16 10:34:39 +0000

    Comentários

--- Menus.h
+++ Menus.h
@@ -3,60 +3,76 @@
 
 #include "Competition.h"
 #include <string>
-
+/// \brief A table with information of all players
+/// \return if 0 menu opens again otherwise go back
 int menu_allPlayers();
 /// \brief Menu for searching Players by Name or Position.
-/// \return if 0, menu_searchPlayers(), menu_Players() otherwise
+/// \return if 0 menu opens again otherwise go back
 int menu_searchPlayers();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_addPlayer();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_updatePlayers();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_removePlayer();
 /// \brief All Player information is here.
-/// \return if 0, menu_Players(), mainMenu() otherwise
+/// \return if 0 menu opens again otherwise go back
 int menu_players();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_allStaff();
 /// \brief Menu for searching Staff Members by Name or Position.
-/// \return if 0, menu_searchStaffMembers(), menu_staff() otherwise
+/// \return if 0 menu opens again otherwise go back
 int menu_searchStaffMembers();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_addStaff();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_updateStaff();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_removeStaff();
 /// \brief All Staff Member information is here.
-/// \return creates or views Staff Members, or returns to the main menu
+/// \return if 0 menu opens again otherwise go back
 int menu_staff();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_searchGame();
-
+/// \brief
 /// \brief All Games information is here.
-/// \return if 0, menu_games(), menu_tournaments() otherwise
+/// \return if 0 menu opens again otherwise go back
 int menu_games();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_payCompetitions();
 /// \brief All Tournament information is here.
-/// \return if 0, menu_tournaments(), mainMenu() otherwise
+/// \return if 0 menu opens again otherwise go back
 int menu_competitionGames(Competition * comp);
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_calledCompetition(Competition* comp);
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_detailedCompetition(Competition * comp);
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_chooseCompetition();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_creatCompetition();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_updateCompetition();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_removeCompetition();
-
+/// \brief
+/// \return if 0 menu opens again otherwise go back
 int menu_tournaments();
-
 /// \brief This menu indicates a Team's stats.
 /// \return if 0, menu_info(), mainMenu() otherwise
 int menu_info();
